f20d646e0c09021533dc67db665275fd1d77e2d8
wchamber01 <52251104+wchamber01@users.noreply.github.com>
2019-11-07 23:26:25 -0500

    got app.js buttons working to show/hide divs on click

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import PodDisplay from './components/APOD/PodDisplay'
 import Rover from './components/Insight/Rover'
 import { Button } from 'reactstrap';
 
 function App() {
-
+  const [showPodDiv, setShowPodDiv] = useState(false);
+  const [showRoverDiv, setShowRoverDiv] = useState(false);
+  
   return (
     <div className="App">
       <div>
         <h1>Welcome to my NASA API Project!</h1>
-        <Button color="primary" size="lg" block>Astronomy Picture of the Day</Button>
-        <div className="PodDisplay"><PodDisplay /></div>
-        <Button color="secondary" size="lg" block>View Photos of Mars from Surface Rovers</Button>
-        <div className="Rover"><Rover /></div>
-        
+        <Button onClick={() => setShowPodDiv(!showPodDiv)} color="primary" size="lg" block>Astronomy Picture of the Day</Button>
+        {showPodDiv && <div className="PodDisplay"><PodDisplay /></div>}
+        <Button onClick={() => setShowRoverDiv(!showRoverDiv)} color="secondary" size="lg" block>View Photos of Mars from Surface Rovers</Button>
+        {showRoverDiv && <div className="Rover"><Rover /></div>}
       </div>
     </div>
   );
